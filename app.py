02af89899f84550df05da90a46034718e5fa216b
gpmahanta <gpm.mahanta@gmail.com>
2019-12-22 21:18:16 +0530

    Signed-off-by: gpmahanta <gpm.mahanta@gmail.com>

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_genre(genre):
             resp = [{"genre":"Horror","all":50,"few":[{"name":"Star Wars","url":"http://stream.starwars","posterUrl":"http://pic","views":23,"likes":13,"language":"english","comments":[{"userName":"Rey","comment":"Star Blood"}]}]},{"genre":"Action","all":50,"few":[{"name":"Jumanji","url":"http://stream.jumanji","posterUrl":"http://pic","views":67,"likes":55,"language":"english","comments":[{"userName":"Switch","comment":"punchy"}]}]}]
 
     response = app.response_class(
-        response=json.dumps(resp),
+        response=jsonify(resp),
         status=200,
         mimetype='application/json'
     )
@@ -75,7 +75,7 @@ def get_language():
             resp = [{"genre":"Horror","all":50,"few":[{"name":"Star Wars","url":"http://stream.starwars","posterUrl":"http://pic","views":23,"likes":13,"language":"english","comments":[{"userName":"Rey","comment":"  Star Blood"}]}]},{"genre":"Action","all":50,"few":[{"name":"Jumanji","url":"http://stream.jumanji","posterUrl":"http://pic","views":67,"likes":55,"language":"english","comments":[{"userName":"Switch","comment":"punchy"}]}]}]
 
     response = app.response_class(
-        response=json.dumps(resp),
+        response=jsonify(resp),
         status=200,
         mimetype='application/json'
     )
