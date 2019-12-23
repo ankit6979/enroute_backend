2aedcc6b9fefb6f97905b7317787c41ebf133d1e
ankit6979 <ankitkumar.6979@gmail.com>
2019-12-23 19:31:02 +0530

    Changed db structure

---
+++ baseData.py
@@ -0,0 +1,49 @@
+/* MediaObject */
+{
+    "_id" : "",
+    "Name" : "",
+    "Language" : "",
+    "Genre" : "",
+    "url" : "",
+    "Views" : "",
+    "Viewers" : [],
+    "Likes" : "",
+    "PeopleLiked" : [],
+    "Comments" : [ 
+        {}
+    ],
+    "Description" : ""
+}
+
+/* UserCredential1 */
+{
+    "_id" : ObjectId("5df3a9dd62546f7dacd2776c"),
+    "PNR" : "1024",
+    "Name" : "Raj",
+    "SeatNo" : "11F"
+}
+
+/* UserCredential2 */
+{
+    "_id" : ObjectId("5df3aa1162546f7dacd2776d"),
+    "PNR" : "1254",
+    "Name" : "Shilpi",
+    "SeatNo" : "22A"
+}
+
+/* UserCredential3 */
+{
+    "_id" : ObjectId("5e00a985db44501ad96a012b"),
+    "PNR" : "1984",
+    "Name" : "Arpan",
+    "SeatNo" : "18B"
+}
+
+/* UserData */
+{
+    "_id" : ObjectId("5df3963b62546f7dacd2776a"),
+    "PNR" : "",
+    "Name" : "",
+    "Lang_pref" : [],
+    "Genre_pref" : []
+}
