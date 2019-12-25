be16f7f9594cf2429d620f618a1a4f616ee900cf
gpmahanta <gpm.mahanta@gmail.com>
2019-12-25 20:16:08 +0530

    Bug fixes 2

--- app.py
+++ app.py
@@ -3,51 +3,7 @@ from flask import Flask, request, jsonify
 import json
 # from mongodb_functions import *
 
-
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    name = request.form.get('name')
-    print(name)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if name:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+app = Flask(__name__)
 
 @app.route("/genres", defaults={'genre': None}, methods=['GET'])
 def get_genre(genre):
@@ -65,10 +21,10 @@ def get_genre(genre):
     )
     return response
 
-@app.route("/languages", defaults={'language': None}, methods=['GET'])
-def get_language(language):
+@app.route("/languages", defaults={'languages': None}, methods=['GET'])
+def get_language():
     if request.method == 'GET':
-        language = request.args.get('language')
+        language = request.args.get('languages')
         if language == None:
             resp = ['English', 'Hindi']
         else:
@@ -81,7 +37,6 @@ def get_language(language):
     )
     return response
 
-app = Flask(__name__)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
