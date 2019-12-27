7a0299fe1fe00d3c622ede1ffa9d2128d3397c36
Girija Mahanta(girija.mahanta) <girija.mahanta@infosys.com>
2019-12-27 10:27:08 +0530

    Updated connection string

--- mongodb_functions.py
+++ mongodb_functions.py
@@ -1,69 +1,90 @@
+import os
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb+srv://infy:infy123@cluster0-kfqgc.mongodb.net/digital_signage?retryWrites=true&w=majority")
+user = os.environ('USER')
+passwd = os.environ('PASS')
+
+myclient = pymongo.MongoClient("mongodb+srv://" + user + ":" + passwd +
+                               "@cluster0-kfqgc.mongodb.net/CentralEntertainment?retryWrites=true&w=majority")
 mydb = myclient["CentralEntertainment"]
 mycol = mydb["item"]
 
+
 def AddContent(pnr, media_id, viewflag, likeflag, comments, anonymousflag):
-   if(anonymousflag):
-      pnr = "Anonymous"
-   returnVal = mydb['MediaObject'].update_one({"_id":media_id}, {"$inc":{"Likes":likeflag, "Views":viewflag}, "$push":{"Viewers":pnr, "PeopleLiked":pnr, "Comments":{"pnr":pnr, "comments":comments}}})   
-   return {"result": returnVal.matched_count>0}
+    if(anonymousflag):
+        pnr = "Anonymous"
+    returnVal = mydb['MediaObject'].update_one({"_id": media_id}, {"$inc": {"Likes": likeflag, "Views": viewflag}, "$push": {
+                                               "Viewers": pnr, "PeopleLiked": pnr, "Comments": {"pnr": pnr, "comments": comments}}})
+    return {"result": returnVal.matched_count > 0}
+
 
 def userPreference(pnr, name, lang_pref, genre_pref):
-   val = mydb['UserData'].update_one({"PNR":pnr, "Name":name}, {"$set": {"Lang_pref":lang_pref, "Genre_pref":genre_pref}})
-   return {"result":val.matched_count>0}
+    val = mydb['UserData'].update_one({"PNR": pnr, "Name": name}, {
+                                      "$set": {"Lang_pref": lang_pref, "Genre_pref": genre_pref}})
+    return {"result": val.matched_count > 0}
+
 
 def checkUser(pnr, name, seatno):
-   returnVal = mydb['UserCredential'].find({"Name":name, "PNR":pnr, "SeatNo":seatno}).count() > 0
-   return {"result":returnVal}
+    returnVal = mydb['UserCredential'].find(
+        {"Name": name, "PNR": pnr, "SeatNo": seatno}).count() > 0
+    return {"result": returnVal}
+
 
 def saveData(language, genre, name, url):
-   base_document = {"_id": 1, "lang": {"hindi": {"sports": {}, "entertainment": {}, "documentry": {}, "news": {}}, "english": {"sports": {}, "entertainment": {}, "documentry": {}, "news": {}}}}
+    base_document = {"_id": 1, "lang": {"hindi": {"sports": {}, "entertainment": {}, "documentry": {
+    }, "news": {}}, "english": {"sports": {}, "entertainment": {}, "documentry": {}, "news": {}}}}
 
-   if mycol.estimated_document_count() == 0:
-      resp = mycol.insert_one(base_document)
-      resp = mycol.update_one({"_id" : 1 }, {"$set": {"lang." + language + "." + genre + "." + name : url}})
-   else:
-      resp = mycol.update_one({"_id" : 1 }, {"$set": {
-         "lang." + language + "." + genre + "." + "name" : name,
-         "lang." + language + "." + genre + "." + "url" : url,
-         }})
-   print("saved")
-   return resp 
+    if mycol.estimated_document_count() == 0:
+        resp = mycol.insert_one(base_document)
+        resp = mycol.update_one(
+            {"_id": 1}, {"$set": {"lang." + language + "." + genre + "." + name: url}})
+    else:
+        resp = mycol.update_one({"_id": 1}, {"$set": {
+            "lang." + language + "." + genre + "." + "name": name,
+            "lang." + language + "." + genre + "." + "url": url,
+        }})
+    print("saved")
+    return resp
+
 
 def deleteData(language, genre, name):
-   resp = mycol.update_one({"_id" : 1 }, {"$unset": {"lang." + language + "." + genre + "." + name : 1}})
-   print("deleted")
-   return resp
+    resp = mycol.update_one(
+        {"_id": 1}, {"$unset": {"lang." + language + "." + genre + "." + name: 1}})
+    print("deleted")
+    return resp
+
 
 def queryData(language=None, genre=None):
-   language=None
-   video_list = []
-   if language == None: 
-      resp = mycol.find_one({"_id" : 1}, {"_id" : 0 })
-      lang = resp['lang']
-      for i, j in lang.items():
-         for x,y in j.items():
-            for a,b in y.items():
-               d = {}
-               d['name'] = a
-               d['url'] = b
-               video_list.append(d)
+    language = None
+    video_list = []
+    if language == None:
+        resp = mycol.find_one({"_id": 1}, {"_id": 0})
+        lang = resp['lang']
+        for i, j in lang.items():
+            for x, y in j.items():
+                for a, b in y.items():
+                    d = {}
+                    d['name'] = a
+                    d['url'] = b
+                    video_list.append(d)
 
-   # elif (language != None) & (genre == None):
-   #    resp = mycol.find_one({"_id" : 1}, {"_id" : 0,  "lang" + "." + language : 1})
-   # elif (language != None) & (genre != None):
-   #    resp = mycol.find_one({"_id" : 1}, {"_id" : 0,  "lang" + "." + language + "." + genre : 1})
-   return video_list
+    # elif (language != None) & (genre == None):
+    #    resp = mycol.find_one({"_id" : 1}, {"_id" : 0,  "lang" + "." + language : 1})
+    # elif (language != None) & (genre != None):
+    #    resp = mycol.find_one({"_id" : 1}, {"_id" : 0,  "lang" + "." + language + "." + genre : 1})
+    return video_list
+
 
 def queryLanguage(language):
-   resps = mydb['MediaObject'].find({"Language":language}, {"_id":1, "Name":1, "Genre":1, "url":1, "ThumbnailUrl":1 })
-   return [resp for resp in resps]
+    resps = mydb['MediaObject'].find({"Language": language}, {
+                                     "_id": 1, "Name": 1, "Genre": 1, "url": 1, "ThumbnailUrl": 1})
+    return [resp for resp in resps]
+
 
 def queryGenre(genre):
-   resps = mydb['MediaObject'].find({"Genre":genre}, {"_id":1, "Name":1, "url":1, "ThumbnailUrl":1 })
-   return [resp for resp in resps]
+    resps = mydb['MediaObject'].find(
+        {"Genre": genre}, {"_id": 1, "Name": 1, "url": 1, "ThumbnailUrl": 1})
+    return [resp for resp in resps]
 
 #docs = queryLanguage("English")
 #print([doc for doc in docs])
